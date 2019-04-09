a5a8be7e0804e1c54732fdf87e1d081c1e20e24c
Alexa VanHattum <amv94@cornell.edu>
2019-04-08 22:47:15 -0400

    new functions for each partition

--- src/emit_llvm.ml
+++ src/emit_llvm.ml
@@ -2,6 +2,13 @@ open Dfg
 open Ast
 open Partition
 
+let compare_partition_funs (p1, f1) (p2, f2) =
+  let same_partitions = compare p1 p2 in
+  if same_partitions != 0 then same_partitions else compare f1 f2
+
+module NewFunctionMap =
+  Map.Make(struct type t = int * Llvm.llvalue;; let compare = compare_partition_funs end)
+
 let format_partition (p, (t1, t2)) =
   (string_of_int p) ^ ", ("  ^ (string_of_int t1) ^ ", " ^ (string_of_int t2) ^ ")"
 
@@ -17,25 +24,39 @@ let emit_llvm (dfg : partitioning) (llvm_to_ast : (Llvm.llvalue * com) list) (no
 
   let context = (Llvm.create_context ()) in
   let new_module = Llvm.create_module context "new_module" in
-  let void = (Llvm.void_type context) in
-  let fun_type = Llvm.function_type void (Array.make 3 (Llvm.double_type context)) in
-  let part_fun = Llvm.define_function "partition" fun_type new_module in
-  let fun_begin = Llvm.instr_begin (Llvm.entry_block part_fun) in
-  let builder = Llvm.builder_at context fun_begin in
+  let new_funs = ref NewFunctionMap.empty in
 
-  let add_instruction (v, p) =
-    Printf.printf "%s -> %s\n" (format_partition p) (Llvm.string_of_llvalue v);
+  let add_instruction (v, (p, (_, _))) =
     match (Llvm.classify_value v) with
     | Instruction _ ->
-      Printf.printf "parent: %s\n" (Llvm.string_of_llvalue (Llvm.value_of_block (Llvm.instr_parent v)));
-      Llvm.insert_into_builder (Llvm.instr_clone v) "" builder;
-    | _ -> ()
+      let parent_fun = (Llvm.block_parent (Llvm.instr_parent v)) in
+      let key = (p, parent_fun) in
+      let insts_opt = NewFunctionMap.find_opt key !new_funs in
+      let insts = begin match insts_opt with | Some l -> l | None -> [] end in
+      new_funs := NewFunctionMap.add key (v::insts) !new_funs
+    | _ -> Printf.printf "not instruction: %s\n" (Llvm.string_of_llvalue v);
   in
 
   List.iter add_instruction sorted;
+
+  let construct_new_functions (p, f) insts =
+    let t = Llvm.type_of f in
+    let fun_type = Llvm.function_type (Llvm.return_type t) (Llvm.param_types t) in
+    let new_name = (Llvm.value_name f) ^ "_" ^ (string_of_int p) in
+    let part_fun = Llvm.define_function new_name fun_type new_module in
+    let fun_begin = Llvm.instr_begin (Llvm.entry_block part_fun) in
+    let builder = Llvm.builder_at context fun_begin in
+    List.iter (fun i -> Llvm.insert_into_builder (Llvm.instr_clone i) "" builder) (List.rev insts);
+  in
+
+  NewFunctionMap.iter construct_new_functions !new_funs;
+
   Llvm.print_module "llvm_out.ll" new_module
 
 (*
+
+        Llvm.insert_into_builder (Llvm.instr_clone v) "" builder;
+
 val define_function : string -> lltype -> llmodule -> llvalue
 val create_module : llcontext -> string -> llmodule
 val create_context : unit -> llcontext
