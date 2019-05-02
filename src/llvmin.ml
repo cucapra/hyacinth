78f3dda888114e6fea2f42856d0fed176f2d9856
Alexa VanHattum <amv94@cornell.edu>
2019-05-02 14:44:46 -0400

    Add more opcodes, add (nonfunctional) matrix multiply example

--- src/llvmin.ml
+++ src/llvmin.ml
@@ -64,31 +64,78 @@ let opcode_cost (opcode : Opcode.t) : int =
   | Store -> 1
   | FMul -> 5
   | FSub -> 3
-  | FDiv -> 10
+  | FDiv -> 7
   | FAdd -> 3
   | FCmp -> 1
   | PHI -> 0
   | Ret -> 1
-  | Call -> 10
+  | Call -> 15
   | Br -> 1
   | Select -> 1
-  | _ -> 10
+  | _ -> 7
 
 let pretty_opcode (opcode : Opcode.t) : string =
   match opcode with
+  | Invalid -> "invalid"
+  | Ret -> "ret"
+  | Br -> "br"
+  | Switch -> "switch"
+  | IndirectBr -> "indirectbr"
+  | Invoke -> "invoke"
+  | Invalid2 -> "invalid2"
+  | Unreachable -> "unreachable"
+  | Add -> "add"
+  | FAdd -> "fadd"
+  | Sub -> "sub"
+  | FSub -> "fsub"
+  | Mul -> "mul"
+  | FMul -> "fmul"
+  | UDiv -> "udiv"
+  | SDiv -> "sdiv"
+  | FDiv -> "fdiv"
+  | URem -> "urem"
+  | SRem -> "srem"
+  | FRem -> "frem"
+  | Shl -> "shl"
+  | LShr -> "lshr"
+  | AShr -> "ashr"
+  | And -> "and"
+  | Or -> "or"
+  | Xor -> "xor"
   | Alloca -> "alloca"
   | Load -> "load"
   | Store -> "store"
-  | FMul -> "fmul"
-  | FSub -> "fsub"
-  | FDiv -> "fdiv"
-  | FAdd -> "fadd"
+  | GetElementPtr -> "getelementptr"
+  | Trunc -> "trunc"
+  | ZExt -> "zext"
+  | SExt -> "sext"
+  | FPToUI -> "fptoui"
+  | FPToSI -> "fptosi"
+  | UIToFP -> "uitofp"
+  | SIToFP -> "sitofp"
+  | FPTrunc -> "fptrunc"
+  | FPExt -> "fpext"
+  | PtrToInt -> "ptrtoint"
+  | IntToPtr -> "inttoptr"
+  | BitCast -> "bitcast"
+  | ICmp -> "icmp"
   | FCmp -> "fcmp"
   | PHI -> "phi"
-  | Ret -> "ret"
   | Call -> "call"
-  | Br -> "br"
   | Select -> "select"
+  | UserOp1 -> "userop1"
+  | UserOp2 -> "userop2"
+  | VAArg -> "vaarg"
+  | ExtractElement -> "extractelement"
+  | InsertElement -> "insertelement"
+  | ShuffleVector -> "shufflevector"
+  | ExtractValue -> "extractvalue"
+  | InsertValue -> "insertvalue"
+  | Fence -> "fence"
+  | AtomicCmpXchg -> "atomiccmpxchg"
+  | AtomicRMW -> "atomicrmw"
+  | Resume -> "resume"
+  | LandingPad -> "landingpad"
   | _ -> "other"
 
 let print_fun lv =
